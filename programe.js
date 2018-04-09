4d4a4a81bdf18fa90f01a2c4ca9507a2f448547f
Marcin Różański <rozanskimarcin2016@gmail.com>
2018-04-09 23:54:25 +0200

    Add content

---
+++ programe.js
@@ -0,0 +1,18 @@
+process.stdin.setEncoding('utf-8');
+process.stdin.on('readable', function() {
+	var input = process.stdin.read();
+	if (input !== null) {
+		var instruction = input.toString().trim();
+		switch(instruction) {
+			case '/exit':
+				process.stdout.write('Quitting app!\n');
+				process.exit();
+				break;
+			case '/version': 
+				process.stderr.write('The node version is ' + process.versions.node + '\n');
+				break;
+			default:
+				process.stderr.write('Wrong instruction!');
+		}
+	}
+});
