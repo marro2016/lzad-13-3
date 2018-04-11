34464191612ecc45309314e917582677a2d64d55
Marcin Różański <rozanskimarcin2016@gmail.com>
2018-04-11 22:59:06 +0200

    corrected

--- programe.js
+++ programe.js
@@ -1,18 +1,19 @@
-rocess.stdin.setEncoding('utf-8');
+process.stdin.setEncoding('utf-8');
+console.log('App started, please write instruction below...');
 process.stdin.on('readable', function() {
-      var input = process.stdin.read();
-      if (input !== null) {
-        var instruction = input.toString().trim();
-        switch (instruction) {
-          case '/exit':
-            process.stdout.write('Quitting app!\n');
-            process.exit();
-            break;
-          case '/version':
-            process.stderr.write('The node version is ' + p$
-              break;
-              default:
-              process.stderr.write('Wrong instruction!');
-            }
-        }
-      });
+  var input = process.stdin.read();
+  if (input !== null) {
+    var instruction = input.toString().trim();
+    switch (instruction) {
+      case '/exit':
+        process.stdout.write('Quitting app!\n');
+        process.exit();
+        break;
+      case '/version':
+        process.stderr.write('The node version is ' + process.versions.node + '\n');
+        break;
+      default:
+        process.stderr.write('Wrong instruction!');
+    }
+  }
+});
